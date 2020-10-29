b24db6b2a8766b2875f55e3e6df12c3251341dd5
bregarulai <bregarulai@gmail.com>
2020-10-28 21:55:21 -0400

    created query to get jobs data from strapi

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -4,7 +4,26 @@ import { FaAngleDoubleRight } from "react-icons/fa"
 import { graphql, useStaticQuery } from "gatsby"
 import { Link } from "gatsby"
 
+const query = graphql`
+  {
+    allStrapiJobs(sort: { fields: strapiId, order: DESC }) {
+      nodes {
+        company
+        date
+        position
+        desc {
+          id
+          name
+        }
+        strapiId
+      }
+    }
+  }
+`
+
 const Jobs = () => {
+  const data = useStaticQuery(query)
+
   return <h2>jobs component</h2>
 }
 
