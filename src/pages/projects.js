b9e7ead49b94a07af155878d6c3a70713f717896
bregarulai <bregarulai@gmail.com>
2020-11-01 13:54:20 -0500

    implemented projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,10 +2,45 @@ import React from "react"
 import Layout from "../components/Layout"
 import { graphql } from "gatsby"
 import Projects from "../components/Projects"
-// ...GatsbyImageSharpFluid
 
-const ProjectsPage = () => {
-  return <h2>projects page</h2>
+const ProjectsPage = ({
+  data: {
+    allStrapiProjects: { nodes: projects },
+  },
+}) => {
+  return (
+    <Layout>
+      <section className="projects-page">
+        <Projects projects={projects} title="all projects" />
+      </section>
+    </Layout>
+  )
 }
+
+export const query = graphql`
+  {
+    allStrapiProjects {
+      nodes {
+        strapiId
+        description
+        featured
+        github
+        url
+        stack {
+          id
+          title
+        }
+        title
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default ProjectsPage
