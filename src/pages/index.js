5f1b86b976806a1deadbc3c412eee7dfb8f86aa9
bregarulai <bregarulai@gmail.com>
2020-11-01 15:22:02 -0500

    created query to get data from strapi for blogs component

--- src/pages/index.js
+++ src/pages/index.js
@@ -43,5 +43,23 @@ export const query = graphql`
         }
       }
     }
+    allStrapiBlogs(sort: { fields: date, order: DESC }, limit: 3) {
+      nodes {
+        strapiId
+        slug
+        title
+        description
+        content
+        date(formatString: "MMMM Do, YYYY")
+        category
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
   }
 `
