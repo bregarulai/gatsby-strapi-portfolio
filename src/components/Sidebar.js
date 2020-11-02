3818ec0f04479b7a6f985807b8b488c49efe2561
bregarulai <bregarulai@gmail.com>
2020-11-02 12:43:30 -0500

    fixed animation on sidebar component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -13,8 +13,8 @@ const Sidebar = ({ toggleSideBar, isOpen }) => {
         <FaTimes />
       </button>
       <div className="side-container">
-        <Links styleClass="sidebar-links" />
-        <SocialLinks styleClass="sidebar-icons" />
+        <Links styleClass={`${isOpen ? "sidebar-links" : ""}`} />
+        <SocialLinks styleClass={`${isOpen ? "sidebar-icons" : ""}`} />
       </div>
     </aside>
   )
